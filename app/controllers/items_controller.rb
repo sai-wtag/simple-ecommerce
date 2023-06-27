e14a96a931acbf552deb9827037e9036f5e520e1
Md Asiful Islam Sakib <asiful.sakib@welldev.io>
2023-06-27 15:56:50 +0600

    chore: Add avaiable quantity as input

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -52,7 +52,7 @@ class ItemsController < ApplicationController
 
   private
     def item_params
-      params.require(:item).permit(:name, :description, :price)
+      params.require(:item).permit(:name, :description, :price, :available_quantity)
     end
 
 end
