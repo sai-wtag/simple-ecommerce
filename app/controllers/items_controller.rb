657587048788c39c13ae1c81c6c19bc96422f71d
Md Asiful Islam Sakib <asiful.sakib@welldev.io>
2023-06-27 15:04:12 +0600

    chore: Add functionality to edit item

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,12 +5,12 @@ class ItemsController < ApplicationController
     @items = Item.all.order(created_at: :desc)
   end
 
-  def new
-    @item = Item.new
-  end
-
   def show
     @item = Item.find(params[:id])
+  end
+
+  def new
+    @item = Item.new
   end
 
   def create
@@ -22,6 +22,21 @@ class ItemsController < ApplicationController
       flash[:error] = "Something went wrong"
       render 'new'
     end
+  end
+  
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    @item = Item.find(params[:id])
+      if @item.update(item_params)
+        flash[:success] = "Item was successfully updated"
+        redirect_to @item
+      else
+        flash[:error] = "Something went wrong"
+        render :edit, status: :unprocessable_entity
+      end
   end
 
   private
