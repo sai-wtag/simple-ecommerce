0d9f1834a72c36e70cf1c36de589b449abd73a3e
Md Asiful Islam Sakib <asiful.sakib@welldev.io>
2023-06-27 15:07:36 +0600

    fix: Remove 'only_integer' validation from 'item' model

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,5 +2,5 @@ class Item < ApplicationRecord
   validates :name, :description, :price, presence:true
 
   validates :name, length: { minimum: 3, maximum: 50 }
-  validates :price, numericality: { greater_than: 0, only_integer: true }
+  validates :price, numericality: { greater_than: 0 }
 end
