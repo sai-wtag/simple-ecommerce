9026f1be0a0fd16e90773ba12246b7c2553e1242
Md Asiful Islam Sakib <asiful.sakib@welldev.io>
2023-06-27 18:12:25 +0600

    chore: Add order form

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -5,4 +5,16 @@ class OrdersController < ApplicationController
     @items = Item.all.order(created_at: :desc)
     @order = Order.new
   end
+
+  def create
+    @order = Order.new(order_params)
+    if @order.save
+      @order.update(user_id: current_user.id)
+      flash[:success] = "Order successfully created"
+      redirect_to @order
+    else
+      flash[:error] = "Something went wrong"
+      render 'new'
+    end
+  end
 end
