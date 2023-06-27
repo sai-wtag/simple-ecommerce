bf72c519c0a1a8d9a1e2ba439ea3bfba1fcb55ce
Md Asiful Islam Sakib <asiful.sakib@welldev.io>
2023-06-27 11:42:53 +0600

    chore: Migrate items table

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_06_21_061136) do
+ActiveRecord::Schema.define(version: 2023_06_27_054101) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "items", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.decimal "price"
+    t.integer "available_quantity"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name", null: false
