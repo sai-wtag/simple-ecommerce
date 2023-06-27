30063399c0d363d6711d9c7c60d0809d0cdb1c89
Md Asiful Islam Sakib <asiful.sakib@welldev.io>
2023-06-27 12:07:08 +0600

    chore: Add functionality to create new item

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,4 +5,28 @@ class ItemsController < ApplicationController
     @items = Item.all.order(created_at: :desc)
   end
 
+  def new
+    @item = Item.new
+  end
+
+  def show
+    @item = Item.find(params[:id])
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      flash[:success] = "Item successfully created"
+      redirect_to @item
+    else
+      flash[:error] = "Something went wrong"
+      render 'new'
+    end
+  end
+
+  private
+    def item_params
+      params.require(:item).permit(:name, :description, :price)
+    end
+
 end
