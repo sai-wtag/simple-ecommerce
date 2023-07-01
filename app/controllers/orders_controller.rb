6d8b2cff48c45d6ba2018190d58b76028f4617c7
Asiful Islam Sakib <122719191+sai-wtag@users.noreply.github.com>
2023-07-02 02:32:38 +0600

    chore: Save order

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -7,14 +7,33 @@ class OrdersController < ApplicationController
   end
 
   def create
-    @order = Order.new(order_params)
-    if @order.save
-      @order.update(user_id: current_user.id)
-      flash[:success] = "Order successfully created"
-      redirect_to @order
-    else
-      flash[:error] = "Something went wrong"
-      render 'new'
+    item_ids = params[:order][:item_ids]
+    quantities = params[:order][:quantities]
+
+    items = Hash.new(0)
+
+    quantities.each_with_index do |quantity, index|
+      if quantity.to_i > 0
+        items[item_ids[index]] = quantity
+      end
     end
+
+    available_items = Item.where(id: items.keys)
+    total_price = 0
+    
+    available_items.each do |item|
+      if item.available_quantity < items[item.id.to_s].to_i
+        flash[:error] = "Not enough #{item.name} in stock"
+        redirect_to new_order_path
+
+        return
+      end
+
+      total_price += item.price * items[item.id.to_s].to_i
+    end
+
+    order = Order.new(user_id: current_user.id, total_price: total_price)
+    order.save
+
   end
 end
