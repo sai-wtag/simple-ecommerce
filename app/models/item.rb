28bc17ba842c5492ae6d9f7a8f5d771d3bb9e4db
Asiful Islam Sakib <asiful.sakib@welldev.io>
2023-07-02 14:07:09 +0600

    fix: Add relation to Item model

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,8 @@
 class Item < ApplicationRecord
-  validates :name, :description, :price, presence:true
+  has_many :order_items, dependent: :destroy
+  has_many :orders, through: :order_items
 
+  validates :name, :description, :price, presence: true
   validates :name, length: { minimum: 3, maximum: 50 }
   validates :price, numericality: { greater_than: 0 }
 end
