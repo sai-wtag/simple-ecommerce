67331c99f86799e6d90e1f01abfe3eb67ba754cf
Asiful Islam Sakib <asiful.sakib@welldev.io>
2023-07-02 11:05:02 +0600

    chore: Add migration for OrderItem

--- db/migrate/20230701203427_create_order_items.rb
+++ db/migrate/20230701203427_create_order_items.rb
@@ -2,6 +2,10 @@ class CreateOrderItems < ActiveRecord::Migration[6.1]
   def change
     create_table :order_items do |t|
 
+      t.references :order, null: false, foreign_key: true, on_delete: :cascade
+      t.references :item, null: false, foreign_key: true
+      t.integer :quantity, null: false
+      
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_06_27_103904) do
+ActiveRecord::Schema.define(version: 2023_07_01_203427) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,16 @@ ActiveRecord::Schema.define(version: 2023_06_27_103904) do
     t.integer "available_quantity", default: 0
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "order_items", force: :cascade do |t|
+    t.bigint "order_id", null: false
+    t.bigint "item_id", null: false
+    t.integer "quantity", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["item_id"], name: "index_order_items_on_item_id"
+    t.index ["order_id"], name: "index_order_items_on_order_id"
   end
 
   create_table "orders", force: :cascade do |t|
@@ -48,5 +58,7 @@ ActiveRecord::Schema.define(version: 2023_06_27_103904) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "order_items", "items"
+  add_foreign_key "order_items", "orders"
   add_foreign_key "orders", "users"
 end
