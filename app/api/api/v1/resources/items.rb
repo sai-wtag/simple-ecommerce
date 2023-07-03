23f11d77b1297bd099387b4e08e930bc82716e7e
Asiful Islam Sakib <asiful.sakib@welldev.io>
2023-07-03 18:42:13 +0600

    chore: Complete CRUD for items API

--- app/api/api/v1/resources/items.rb
+++ app/api/api/v1/resources/items.rb
@@ -8,7 +8,7 @@ module V1
       resource :items do
         desc "Return list of items"
         get do
-          items = Item.all
+          items = Item.all.order(created_at: :asc)
           present items, with: V1::Entities::Item
         end
 
@@ -18,6 +18,52 @@ module V1
         end
         get ':id' do
           item = Item.find(params[:id])
+          present item, with: V1::Entities::Item
+        end
+
+        desc "Create a new item"
+        params do
+          requires :name, type: String, desc: "Item name"
+          requires :description, type: String, desc: "Item description"
+          requires :price, type: Float, desc: "Item price"
+          requires :available_quantity, type: Integer, desc: "Item available quantity"
+        end
+        post do
+          item = Item.create!({
+            name: params[:name],
+            description: params[:description],
+            price: params[:price],
+            available_quantity: params[:available_quantity]
+          })
+          present item, with: V1::Entities::Item
+        end
+
+        desc "Update an existing item"
+        params do
+          requires :id, type: Integer, desc: "Item id"
+          requires :name, type: String, desc: "Item name"
+          requires :description, type: String, desc: "Item description"
+          requires :price, type: Float, desc: "Item price"
+          requires :available_quantity, type: Integer, desc: "Item available quantity"
+        end
+        put ':id' do
+          item = Item.find(params[:id])
+          item.update({
+            name: params[:name],
+            description: params[:description],
+            price: params[:price],
+            available_quantity: params[:available_quantity]
+          })
+          present item, with: V1::Entities::Item
+        end
+
+        desc "Delete an existing item"
+        params do
+          requires :id, type: Integer, desc: "Item id"
+        end
+        delete ':id' do
+          item = Item.find(params[:id])
+          item.destroy
           present item, with: V1::Entities::Item
         end
       end
