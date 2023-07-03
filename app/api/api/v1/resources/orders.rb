9185d6a3a96f8a5fda9027d093f2c031b1240e47
Asiful Islam Sakib <asiful.sakib@welldev.io>
2023-07-03 19:42:41 +0600

    chore: Add functionality to fetch my orders & cancel order

--- app/api/api/v1/resources/orders.rb
+++ app/api/api/v1/resources/orders.rb
@@ -5,6 +5,8 @@ module V1
       format :json
       prefix :api
 
+      $user_id = 2
+
       resource :orders do
         desc "Return list of orders"
         get do
@@ -20,6 +22,29 @@ module V1
           order = Order.find(params[:id])
           present order, with: V1::Entities::Order
         end
+      end
+
+      desc "Return list of my orders"
+      get 'my-orders' do
+        orders = Order.where(user_id: $user_id).order(created_at: :desc)
+        present orders, with: V1::Entities::Order
+      end
+
+      desc "Cancel an order"
+      params do
+        requires :id, type: Integer, desc: "Order id"
+      end
+      put 'cancel-order/:id' do
+        order = Order.find(params[:id])
+
+        if order.order_status != "pending" || order.user_id != $user_id
+          error!('You are not allowed to cancel this order', 403)
+        end
+
+        order.update({
+          order_status: "cancelled"
+        })
+        present order, with: V1::Entities::Order
       end
     end
   end
